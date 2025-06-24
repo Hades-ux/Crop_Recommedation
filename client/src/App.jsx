82a072b25c41dff757cc1496e0e29e400da5bc6d
Hades <vishal1807singh@gmail.com>
2025-06-24 18:12:24 +0530

    frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -25,7 +25,7 @@ function App() {
     setLoading(true);
 
     try {
-      const res = await fetch("http://localhost:5000/predict", {
+      const res = await fetch("https://crop-backend.onrender.com/predict", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
